b162a15a3e296d2d02819e24f80c5e8eee9d1311
Nate <sk8terboyy1104@yahoo.com>
2016-06-15 23:09:22 -0500

    Update trnttwerkbot.js

--- trnttwerkbot.js
+++ trnttwerkbot.js
@@ -3087,7 +3087,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                        API.sendChat('/me This bot was created by ' + botCreator + ', but was edited and is maintained by our Manager, @' + botMaintainer + ".");
+                        API.sendChat('/me This bot was created by ' + botCreator + ', but was edited and is maintained by our Manager, ' + botMaintainer + ".");
                     }
                 }
             },
